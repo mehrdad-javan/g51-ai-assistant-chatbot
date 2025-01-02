641a9a622bdf8cd7e169683e4f76c446b1b92da3
Mehrdad Javan <mehrdad.javan@lexicon.se>
2025-01-02 09:23:17 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/assistantchatbot/G51AssistantChatbotApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.assistantchatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G51AssistantChatbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G51AssistantChatbotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/assistantchatbot/G51AssistantChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.assistantchatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G51AssistantChatbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
